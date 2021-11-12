6f829bd7a718d2e294c0c8362d1b6a06761d9b1c
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-12 17:10:07 +0000

    adds save in member_repository

--- console.py
+++ console.py
@@ -1,3 +1,12 @@
 from models.instructional_event import InstructionalEvent
 from models.member import Member
 from repositories import instructional_event_repository, member_repository
+from datetime import datetime, date
+import os
+
+os.system('psql -d gym -f db/gym.sql')
+
+member1 = Member("Augusta", "Eldrick", date(1997, 5, 19), "crobles@yahoo.ca")
+member_repository.save(member1)
+member2 = Member("Rashad", "Yewande", date(1972, 8, 4), "nogin@icloud.com", "Male")
+member_repository.save(member2)
--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -1,2 +1,16 @@
 from db.run_sql import run_sql
 from models.member import Member
+
+def save(member):
+    sql = """INSERT INTO members (first_name, last_name, dob, email, gender)
+             VALUES (%(first_name)s, %(last_name)s, %(dob)s, %(email)s, %(gender)s)
+             RETURNING *"""
+    values = {'first_name': member.first_name, 
+              'last_name': member.last_name,
+              'dob': member.dob,
+              'email': member.email,
+              'gender': member.gender
+              }
+    result = run_sql(sql, values)[0]
+    member.id = result["id"]
+
