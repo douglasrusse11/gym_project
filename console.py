4cdd666d4e92340f25d4521c65439da8f1f19148
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-12 17:18:52 +0000

    adds save in instructional_event_repository

--- console.py
+++ console.py
@@ -10,3 +10,8 @@ member1 = Member("Augusta", "Eldrick", date(1997, 5, 19), "crobles@yahoo.ca")
 member_repository.save(member1)
 member2 = Member("Rashad", "Yewande", date(1972, 8, 4), "nogin@icloud.com", "Male")
 member_repository.save(member2)
+
+instructional_event1 = InstructionalEvent("Yoga", datetime(2021, 11, 12, 18), 90)
+instructional_event_repository.save(instructional_event1)
+instructional_event2 = InstructionalEvent("Martial Arts", datetime(2021, 11, 12, 19), 60, [member1, member2], 1)
+instructional_event_repository.save(instructional_event2)
--- repositories/instructional_event_repository.py
+++ repositories/instructional_event_repository.py
@@ -1,2 +1,14 @@
 from db.run_sql import run_sql
 from models.instructional_event import InstructionalEvent
+
+def save(instructional_event):
+    sql = """INSERT INTO instructional_events (name, time, duration)
+             VALUES (%(name)s, %(time)s, %(duration)s) 
+             RETURNING *
+             """
+    values = {'name': instructional_event.name,
+              'time': instructional_event.time,
+              'duration': instructional_event.duration
+              }
+    result = run_sql(sql, values)[0]
+    instructional_event.id = result["id"]
