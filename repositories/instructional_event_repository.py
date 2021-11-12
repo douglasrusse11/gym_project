d2e5e5f5ce167b8f14072fbf93a2452fb1a876ef
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-12 17:26:29 +0000

    modifies save in instructional_event_repository to use add_member if instructional_event.members is not empty

--- repositories/instructional_event_repository.py
+++ repositories/instructional_event_repository.py
@@ -12,6 +12,9 @@ def save(instructional_event):
               }
     result = run_sql(sql, values)[0]
     instructional_event.id = result["id"]
+    if instructional_event.members is not []:
+        for member in instructional_event.members:
+            add_member(instructional_event, member)
 
 def add_member(instructional_event, member):
     sql = """INSERT INTO gym (member_id, instructional_event_id)
