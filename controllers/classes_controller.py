b4d22c58499fbfe8f0151036cca58bd0b3416842
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-13 11:45:00 +0000

    modifies /classes route to return upcoming classes

--- controllers/classes_controller.py
+++ controllers/classes_controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
+from repositories import instructional_event_repository
 
 classes_blueprint = Blueprint('classes', __name__)
 
 @classes_blueprint.route('/classes')
 def show_all():
-    return "Classes go here"
+    instructional_events = instructional_event_repository.select_all_upcoming()
+    return f"{[instructional_event.__dict__ for instructional_event in instructional_events]}"
