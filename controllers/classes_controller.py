390712d9c57792ad61ce998fdd71b0acd0e7eb36
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-13 12:01:14 +0000

    adds classes/show_all_upcoming template

--- controllers/classes_controller.py
+++ controllers/classes_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from repositories import instructional_event_repository
 
 classes_blueprint = Blueprint('classes', __name__)
@@ -6,4 +6,4 @@ classes_blueprint = Blueprint('classes', __name__)
 @classes_blueprint.route('/classes')
 def show_all():
     instructional_events = instructional_event_repository.select_all_upcoming()
-    return f"{[instructional_event.__dict__ for instructional_event in instructional_events]}"
+    return render_template("classes/show_all_upcoming.html", instructional_events=instructional_events)
