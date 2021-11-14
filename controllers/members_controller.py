88cf28b6026c4838ab3b1c7d8a7fd8c11bec313d
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-14 11:21:24 +0000

    adds /members [POST] route

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 members_blueprint = Blueprint('members', __name__)
 
 @members_blueprint.route('/members')
 def show_all():
     return "Members go here"
+
+@members_blueprint.route('/members', methods=["POST"])
+def save_member():
+    print(request.form)
+    return "Member added"
 
 @members_blueprint.route('/members/new')
 def add_member():
