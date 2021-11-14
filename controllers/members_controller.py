23b5b519154b1d15157fcd0e9f467fe906c9bd5a
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-14 10:59:27 +0000

    adds /members/new route

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -5,3 +5,7 @@ members_blueprint = Blueprint('members', __name__)
 @members_blueprint.route('/members')
 def show_all():
     return "Members go here"
+
+@members_blueprint.route('/members/new')
+def add_member():
+    return "Add member here"
