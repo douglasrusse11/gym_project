78f48aec10897629fad9b57eebf08fcef7c3d95e
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-14 11:30:29 +0000

    modifies /members [POST] route to save new member

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
+from repositories import member_repository
+from models.member import Member
+from datetime import date
 
 members_blueprint = Blueprint('members', __name__)
 
@@ -8,7 +11,14 @@ def show_all():
 
 @members_blueprint.route('/members', methods=["POST"])
 def save_member():
-    print(request.form)
+    first_name = request.form["first-name"]
+    last_name = request.form["last-name"]
+    year, month, day = (int(element) for element in request.form["dob"].split('-'))
+    dob = date(year, month, day)
+    email = request.form["email"]
+    gender = None if request.form["gender"] == "" else request.form["gender"]
+    member = Member(first_name, last_name, dob, email, gender)
+    member_repository.save(member)
     return "Member added"
 
 @members_blueprint.route('/members/new')
