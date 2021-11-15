e8060437f6559eec3ea1ee5c4132617d73effdc5
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-15 15:13:21 +0000

    adds /members_area/<id> [POST] route

--- controllers/members_area_controller.py
+++ controllers/members_area_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from repositories import member_repository
+from datetime import date
 
 members_area_blueprint = Blueprint('members_area', __name__)
 
@@ -11,6 +12,18 @@ def index():
 def show(id):
     member = member_repository.select(id)
     return render_template("members_area/show.html", member=member)
+
+@members_area_blueprint.route('/members_area/<id>', methods=["POST"])
+def update_member(id):
+    member = member_repository.select(id)
+    member.first_name = request.form["first-name"]
+    member.last_name = request.form["last-name"]
+    year, month, day = (int(element) for element in request.form["dob"].split('-'))
+    member.dob = date(year, month, day)
+    member.email = request.form["email"]
+    member.gender = None if request.form["gender"] == "" else request.form["gender"]
+    member_repository.update(member)
+    return redirect(f'/members_area/{id}')
 
 @members_area_blueprint.route('/members_area/<id>/edit')
 def edit_member(id):
