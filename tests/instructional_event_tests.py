0f26aa9df6ba9f5f5c4134d8b793a38af36559cf
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-15 09:28:49 +0000

    adds unit tests for capacity in InstructionalEvent

--- tests/instructional_event_tests.py
+++ tests/instructional_event_tests.py
@@ -7,7 +7,7 @@ class TestInstructionalEvent(unittest.TestCase):
     def setUp(self):
         self.instructional_event1 = InstructionalEvent("Yoga", datetime(2021, 11, 12, 18), 90)
         self.member = Member("Augusta", "Eldrick", date(1997, 5, 19), "crobles@yahoo.ca")
-        self.instructional_event2 = InstructionalEvent("Martial Arts", datetime(2021, 11, 12, 19), 60, [self.member], 1)
+        self.instructional_event2 = InstructionalEvent("Martial Arts", datetime(2021, 11, 12, 19), 60, [self.member], 5, 1)
 
     def test_instructional_event1_has_name(self):
         self.assertEqual("Yoga", self.instructional_event1.name)
@@ -29,6 +29,13 @@ class TestInstructionalEvent(unittest.TestCase):
 
     def test_instructional_event2_has_id(self):
         self.assertEqual(1, self.instructional_event2.id)
+
+    def test_instructional_event1_capacity_is_None(self):
+        self.assertIsNone(self.instructional_event1.capacity)
+
+    @unittest.skip('')
+    def test_instructional_event2_has_capacity(self):
+        self.assertEqual(5, self.instructional_event2.capacity)
 
     def test_end_time(self):
         self.assertEqual(datetime(2021, 11, 12, 19, 30), self.instructional_event1.end_time())
