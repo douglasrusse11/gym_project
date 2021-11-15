497f044616b5e8c3ba0f2a72c141540908dbc7bb
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-15 14:27:58 +0000

    adds /members_area/<id> route

--- controllers/members_area_controller.py
+++ controllers/members_area_controller.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from repositories import member_repository
 
 members_area_blueprint = Blueprint('members_area', __name__)
 
 @members_area_blueprint.route('/members_area')
 def index():
     return "Members area goes here"
+
+@members_area_blueprint.route('/members_area/<id>')
+def show(id):
+    member = member_repository.select(id)
+    return f"Member {member.full_name()}'s profile goes here"
