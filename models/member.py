53472e56f76b430c32e0398e88dde44f717be610
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-15 10:59:51 +0000

    adds age for Member, passes tests

--- models/member.py
+++ models/member.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Member:
     def __init__(self, first_name, last_name, dob, email, gender=None, id=None):
         self.first_name = first_name
@@ -12,3 +14,11 @@ class Member:
 
     def __eq__(self, other):
         return self.id == other.id
+
+    def age(self):
+        today = date.today()
+        if self.dob.month < today.month:
+            return today.year - self.dob.year
+        elif self.dob.month == today.month and self.dob.day <= today.day:
+            return today.year - self.dob.year
+        return today.year - self.dob.year - 1
