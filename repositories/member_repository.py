e4bdf1d47eb1e4f7523d1425ed411b10916879d4
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-16 13:45:35 +0000

    modifies select in member_repository

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -21,7 +21,7 @@ def select(id):
     values = [id]
     result = run_sql(sql, values)[0]
     if result is not None:
-        member = Member(result['first_name'], result['last_name'], result['dob'], result['email'], result['gender'], int(id))
+        member = Member(result['first_name'], result['last_name'], result['dob'], result['email'], result['gender'], result['id'])
     return member
 
 def update(member):
