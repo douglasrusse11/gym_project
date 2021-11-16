f515d51afa2d78ff089785ccda628f9a68821d12
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-16 16:08:12 +0000

    removes members from param_list for __init__ in InstructionalEvent, removes unnecessary tests, updates setUp in member_tests to adjust dobs

--- tests/instructional_event_tests.py
+++ tests/instructional_event_tests.py
@@ -7,8 +7,8 @@ class TestInstructionalEvent(unittest.TestCase):
     def setUp(self):
         self.instructional_event1 = InstructionalEvent("Yoga", datetime(2021, 11, 12, 18), 90)
         self.member = Member("Augusta", "Eldrick", date(1997, 5, 19), "crobles@yahoo.ca")
-        self.instructional_event2 = InstructionalEvent("Martial Arts", datetime(2021, 11, 12, 19), 60, [self.member], 5, id=1)
-        self.instructional_event3 = InstructionalEvent("Martial Arts", datetime(2021, 11, 12, 19), 60, [self.member, self.member, self.member, self.member, self.member], 5, 40, "Female", 2)
+        self.instructional_event2 = InstructionalEvent("Martial Arts", datetime(2021, 11, 12, 19), 60, 5, id=1)
+        self.instructional_event3 = InstructionalEvent("Martial Arts", datetime(2021, 11, 12, 19), 60, 5, 40, "Female", 2)
 
     def test_instructional_event1_has_name(self):
         self.assertEqual("Yoga", self.instructional_event1.name)
@@ -24,9 +24,6 @@ class TestInstructionalEvent(unittest.TestCase):
 
     def test_instructional_event1_id_is_None(self):
         self.assertIsNone(self.instructional_event1.id)
-
-    def test_instructional_event2_has_members(self):
-        self.assertGreater(len(self.instructional_event2.members), 0)
 
     def test_instructional_event2_has_id(self):
         self.assertEqual(1, self.instructional_event2.id)
@@ -52,13 +49,4 @@ class TestInstructionalEvent(unittest.TestCase):
     def test_end_time(self):
         self.assertEqual(datetime(2021, 11, 12, 19, 30), self.instructional_event1.end_time())
         self.assertEqual(datetime(2021, 11, 12, 20), self.instructional_event2.end_time())
-
-    def test_instructional_event1_has_capacity(self):
-        self.assertTrue(self.instructional_event1.has_capacity())
-    
-    def test_instructional_event3_has_capacity(self):
-        self.assertTrue(self.instructional_event3.has_capacity())
-        
-    def test_instructional_event3_has_capacity(self):
-        self.assertFalse(self.instructional_event3.has_capacity())
     
