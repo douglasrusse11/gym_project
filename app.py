17d5bb2985444a61d2fcda60bf7c3ed8e4441037
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-16 13:18:13 +0000

    modifies /login [POST] route

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from controllers.classes_controller import classes_blueprint
 from controllers.members_controller import members_blueprint
 from controllers.members_area_controller import members_area_blueprint
+from repositories import member_repository
 
 app = Flask(__name__)
 app.register_blueprint(classes_blueprint)
@@ -18,8 +19,14 @@ def login():
 
 @app.route('/login', methods=["POST"])
 def handle_login_request():
-    print(request.form)
-    return "Log in successful"
+    username = request.form["username"]
+    if username == "staff":
+        return redirect('/classes')
+    member = member_repository.select_by_name(username)
+    if member:
+        return redirect(f"/members_area/{member.id}")
+    else:
+        return redirect("/login")
 
 if __name__ == '__main__':
     app.run(debug=True)
